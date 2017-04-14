1c5d409dded00b71a4dc3f4fd5ebca6c6c9e332e
Nil <nilriera2000@gmail.com>
2017-04-14 16:35:08 +0200

    Add files via upload
    Simply the "what is right now the core"

---
+++ binary-to-decimal.py
@@ -0,0 +1,63 @@
+#
+#
+#  __   _    __    __
+# |  \ | |  |__|  |  |
+# |   \| |  |  |  |  |
+# |  | | |  |  |  |  |
+# |  |\  |  |  |  |  |__
+# |__| \_|  |__|  |_____|
+#
+#
+
+print("ONLY NUMBERS!")
+resultat=""
+resta=""
+decimalbinary = ""
+mode = input("'d' Decimal o 'b' Binary\n")
+binarydecimal1 = 0
+# DECIMAL SIDE
+decimalin1 =""
+if mode == ("d"):
+    decimalin = int(input("Input a decimal number\n"))
+    decimalin1 = decimalin
+    a = True
+    while not a == False:
+        resta =str(int(decimalin1 % 2)) + resta
+        decimalin1= decimalin1 / 2
+        
+        # print(resta)       #testing
+        # print(decimalin1)   #testing
+        # print("separador")   #testing
+        if decimalin1 < 1:
+            a = False
+    print("This is your decimal to binary result")
+    print(resta)
+        
+# BINARY SIDE
+elif mode == ("b"):
+    binaryin = input("Input a binary number:\n")
+    i = len(binaryin) -1
+    binarynum = 0
+    binarydecimal = 0
+    binaryintra = ""
+    #
+    # The next While turns arround the input
+    # Example: if you introduce a 1010 to make easy to use the string and numbers
+    # I rotate it to 0101 trying to learn how to make it less botched XDDD
+    #
+    while i>= 0:
+        binaryintra = binaryintra + binaryin[i]
+        i = i -1
+    #
+    # Here I made the binary to decimal, as you can see i use the normal used formula
+    # (1, 0) * 2^X 
+    # X is for the possision of the binary number
+    while not binarynum == len(binaryin):
+        
+        #print(binarynum)        #test
+        binarydecimal1=int(binaryintra[binarynum])*2**binarynum
+        #print(binarydecimal1) #test
+        binarydecimal=binarydecimal+binarydecimal1
+        binarynum = binarynum +1
+    print("This is your binary to decimal result")
+    print(binarydecimal) #result

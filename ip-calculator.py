357dee800bbe6504abe0fdbf5da0d8058857c51b
Nil <nilriera2000@gmail.com>
2017-04-26 20:01:01 +0200

    added ip-calculator, still working on it
    one day it would have a GUI made with pyqt(if i learn how to use it ¬.¬)

---
+++ ip-calculator.py
@@ -0,0 +1,23 @@
+ipin = str(input("entra una ip vàlida/n"))
+##maskin = input("entra una màscara vàlida/n")
+hola = 0
+i = 0
+control = 0
+ip1 = ipin.find(".")
+ip1ok=ipin[:(ip1)]
+print(ip1ok)
+ip2 =(ipin[(ip1+1):].find("."))
+
+ip2ok = ipin[(ip1+1):ip1+1+ip2]
+print(ip2ok)
+
+ip3 = ipin[(ip1+1+ip2+1):].find(".")
+ip3ok= ipin[(ip1+1+ip2+1):ip1+1+ip2+1+ip3]
+print(ip3ok)
+
+ip4ok = ipin[(ip1+1+ip2+1+ip3+1):len(ipin)]
+print(ip4ok)
+
+
+
+

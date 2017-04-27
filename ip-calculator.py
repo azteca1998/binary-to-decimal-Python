539662a359b9f1abe111d0c907ecaf8537659222
Nil <nilriera2000@gmail.com>
2017-04-27 11:19:19 +0200

    added mask

--- ip-calculator.py
+++ ip-calculator.py
@@ -1,8 +1,6 @@
 ipin = str(input("entra una ip vàlida/n"))
-#maskin = input("entra una màscara vàlida/n")
-hola = 0
-i = 0
-control = 0
+macin = input("entra una màscara vàlida/n")
+
 ip1 = ipin.find(".")
 ip1ok=ipin[:(ip1)]
 print(ip1ok)
@@ -19,5 +17,19 @@ ip4ok = ipin[(ip1+1+ip2+1+ip3+1):len(ipin)]
 print(ip4ok)
 
 
+mac1 = macin.find(".")
+mac1ok=macin[:(mac1)]
+print(mac1ok)
+mac2 =(macin[(mac1+1):].find("."))
+
+mac2ok = macin[(mac1+1):mac1+1+mac2]
+print(mac2ok)
+
+mac3 = macin[(mac1+1+mac2+1):].find(".")
+mac3ok= macin[(mac1+1+mac2+1):mac1+1+mac2+1+mac3]
+print(mac3ok)
+
+mac4ok = macin[(mac1+1+mac2+1+mac3+1):len(macin)]
+print(mac4ok)
 
 

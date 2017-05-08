f97ce7cf999c4232b98a360c1abc21de55757b3f
Nil <nilriera2000@gmail.com>
2017-05-08 20:18:58 +0200

    working on it
    working on it

--- ip-calculator.py
+++ ip-calculator.py
@@ -1,37 +1,48 @@
-ipin = str(input("entra una ip vàlida/n"))
-macin = input("entra una màscara vàlida/n")
-
+ipin = str(input("enter a ip"))
+maskin = input("enter a mask" )
+def mask(maskin)
 ip1 = ipin.find(".")
-ip1ok=ipin[:(ip1)]
+ip1ok=int(ipin[:(ip1)])
 print(ip1ok)
 ip2 =(ipin[(ip1+1):].find("."))
 
-ip2ok = ipin[(ip1+1):ip1+1+ip2]
+ip2ok = int(ipin[(ip1+1):ip1+1+ip2])
 print(ip2ok)
 
 ip3 = ipin[(ip1+1+ip2+1):].find(".")
-ip3ok= ipin[(ip1+1+ip2+1):ip1+1+ip2+1+ip3]
+ip3ok= int(ipin[(ip1+1+ip2+1):ip1+1+ip2+1+ip3])
 print(ip3ok)
 
-ip4ok = ipin[(ip1+1+ip2+1+ip3+1):len(ipin)]
+ip4ok = int(ipin[(ip1+1+ip2+1+ip3+1):len(ipin)])
 print(ip4ok)
 
 
-mac1 = macin.find(".")
-mac1ok=macin[:(mac1)]
-print(mac1ok)
-mac2 =(macin[(mac1+1):].find("."))
+mask1 = maskin.find(".")
+mask1ok=int(maskin[:(mask1)])
+print(mask1ok)
+mask2 =(maskin[(mask1+1):].find("."))
 
-mac2ok = macin[(mac1+1):mac1+1+mac2]
-print(mac2ok)
+mask2ok = int(maskin[(mask1+1):mask1+1+mask2])
+print(mask2ok)
 
-mac3 = macin[(mac1+1+mac2+1):].find(".")
-mac3ok= macin[(mac1+1+mac2+1):mac1+1+mac2+1+mac3]
-print(mac3ok)
+mask3 = maskin[(mask1+1+mask2+1):].find(".")
+mask3ok= int(maskin[(mask1+1+mask2+1):mask1+1+mask2+1+mask3])
+print(mask3ok)
 
-mac4ok = macin[(mac1+1+mac2+1+mac3+1):len(macin)]
-print(mac4ok)
+mask4ok =int( maskin[(mask1+1+mask2+1+mask3+1):len(maskin)])
+print(mask4ok)
+print("#############################BINARY###########################")
+print("Ip")
+print(bin(ip1ok))
+print(bin(ip2ok))
+print(bin(ip3ok))
+print(bin(ip4ok))
+print("mask")
+print(bin(mask1ok))
+print(bin(mask2ok))
+print(bin(mask3ok))
+print(bin(mask4ok))z
+ip1lock =bin(ip1ok) - bin(mask1ok)
 
-
-
+print(ip1lock)
 
